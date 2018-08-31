2f8dc2dfdf3520703190e4a6c6ee03b172e23fb9
ralfsantacruz <rsantac1@gmail.com>
2018-08-30 21:29:01 -0700

    added my own folder and initial data/files

---
+++ rafael/config.py
@@ -0,0 +1,7 @@
+### API Keys ###
+
+#google maps key
+api_keys = { 
+            "g_key" : "AIzaSyAZYxv0wjHKxZ1ZsInapq5mOkp8VUUH7pQ",
+            "w_key" : "&AAPID=627fe73e331005c5c18a30f6005cf3f7"
+           }
