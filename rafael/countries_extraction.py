ad1f667ed1be7374c9155847111cca52b39d7f5e
ralfsantacruz <rsantac1@gmail.com>
2018-09-06 00:45:36 -0700

    created loop to extract information from 533 json files from CIA factbook API

--- rafael/countries_extraction.py
+++ rafael/countries_extraction.py
@@ -3,6 +3,6 @@ import ijson
 jsonfile = 'Resources/weekly_json/2007-06-18_factbook.json'
 
 with open(jsonfile, 'rb') as f:
-    country = ijson.items(f, 'countries')
+    country = ijson.items(f, 'countries.mexico.data.economy.industries')
     for x in country:
         print(x)
